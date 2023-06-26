ec63930545ebf72876339167d4719422fe331844
Tim McMackin <timothymcmackin@yahoo.com>
2023-06-26 13:58:44 -0400

    note about no more contracts

--- miningByTick.js
+++ miningByTick.js
@@ -143,6 +143,7 @@ const mineDeliverSell = async (ship, procurementContract) => {
 
       if (updatedContract.contract.deliver[0].unitsRequired <= 0) {
         log(ship.symbol, 'completed contract');
+        // No current way to get a new contract
       } else {
         await fs.promises.writeFile(contractCacheFileName, JSON.stringify(updatedContract, null, 2));
       }

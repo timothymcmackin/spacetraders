5a8ba2ab5265cb9a9209fdf979f040ef1a79cffe
Tim McMackin <timothymcmackin@yahoo.com>
2023-06-30 06:23:34 -0400

    Tweak the loop

--- src/tradeNomad2.js
+++ src/tradeNomad2.js
@@ -17,50 +17,64 @@ const { buy } = require('./utils/marketplaceUtils');
 
 
 const main = async (shipSymbol) => {
+  // TODO control ship
   // if (!controlShip(shipSymbol)) {
   //   console.log('Failed to get control of ship', shipSymbol);
   //   return;
   // }
+  console.log(shipSymbol, 'starting trade loop');
   const ship = await get(`/my/ships/${shipSymbol}`);
   var orders = await getOrders(shipSymbol);
   while (orders === 'tradeNomad') {
-    var sourceWaypointSymbol, targetWaypointSymbol, totalProfit, transactions;
-    // Catch case where there are no profitable trips from this system
-    var trip;
-    while (!trip) {
-      trip = await getMostprofitableTripFromSystem(shipSymbol);
-      if (trip) {
-        sourceWaypointSymbol = trip.sourceWaypointSymbol;
-        targetWaypointSymbol = trip.targetWaypointSymbol;
-        totalProfit = trip.totalProfit;
-        transactions = trip.transactions;
-      } else {
-        // Travel to another system
-        const jumpGateWaypoint = await getDifferentSystemJumpgateWaypoint(ship.nav.systemSymbol);
-        await navigate(ship, jumpGateWaypoint, 'to a random other system looking for a profitable trade route');
-      }
+    try {
+      await tradeLoop(ship);
+    } catch (error) {
+      console.log('Error in tradeLoop for', shipSymbol);
+      console.log(error);
     }
-
-    // Go to source waypoint
-    await navigate(ship, sourceWaypointSymbol, 'to start trade');
-    await post(`/my/ships/${shipSymbol}/dock`);
-    // Buy
-    var totalCost = 0;
-    await transactions.reduce(async (prevPromise, { symbol: tradeSymbol, howManyToBuy }) => {
-      await prevPromise;
-      const transactionCost = await buy(shipSymbol, tradeSymbol, howManyToBuy);
-      totalCost += transactionCost;
-    }, Promise.resolve());
-    // console.log('Bought', totalCost, 'credits of goods.');
-    await post(`/my/ships/${shipSymbol}/orbit`);
-
-    // Go to destination
-    await navigate(ship, targetWaypointSymbol);
-    await post(`/my/ships/${shipSymbol}/dock`);
-    const totalSalePrice = await sellAll(shipSymbol);
-    console.log(`${shipSymbol} bought for ${totalCost} and sold for ${totalSalePrice} for a profit of ${totalSalePrice - totalCost}.`);
     orders = await getOrders(shipSymbol);
   }
+
+  // TODO release ship
+  console.log(shipSymbol, 'stopping trade loop');
+}
+
+const tradeLoop = async (ship) => {
+  var sourceWaypointSymbol, targetWaypointSymbol, totalProfit, transactions;
+  // Catch case where there are no profitable trips from this system
+  var trip;
+  while (!trip) {
+    trip = await getMostprofitableTripFromSystem(ship.symbol);
+    if (trip) {
+      sourceWaypointSymbol = trip.sourceWaypointSymbol;
+      targetWaypointSymbol = trip.targetWaypointSymbol;
+      totalProfit = trip.totalProfit;
+      transactions = trip.transactions;
+    } else {
+      // Travel to another system
+      const jumpGateWaypoint = await getDifferentSystemJumpgateWaypoint(ship.nav.systemSymbol);
+      await navigate(ship, jumpGateWaypoint, 'to a random other system looking for a profitable trade route');
+    }
+  }
+
+  // Go to source waypoint
+  await navigate(ship, sourceWaypointSymbol, 'to start trade');
+  await post(`/my/ships/${ship.symbol}/dock`);
+  // Buy
+  var totalCost = 0;
+  await transactions.reduce(async (prevPromise, { symbol: tradeSymbol, howManyToBuy }) => {
+    await prevPromise;
+    const transactionCost = await buy(ship.symbol, tradeSymbol, howManyToBuy);
+    totalCost += transactionCost;
+  }, Promise.resolve());
+  // console.log('Bought', totalCost, 'credits of goods.');
+  await post(`/my/ships/${ship.symbol}/orbit`);
+
+  // Go to destination
+  await navigate(ship, targetWaypointSymbol);
+  await post(`/my/ships/${ship.symbol}/dock`);
+  const totalSalePrice = await sellAll(ship.symbol);
+  console.log(`${ship.symbol} bought for ${totalCost} and sold for ${totalSalePrice} for a profit of ${totalSalePrice - totalCost}.`);
 }
 
 main(process.env.ACTIVE_SHIP)

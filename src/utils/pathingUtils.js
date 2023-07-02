f76aff0001f0e29d69527ba508fd874578508011
Tim McMackin <timothymcmackin@yahoo.com>
2023-07-02 10:43:30 -0400

    Working dijkstra pathing

--- src/utils/pathingUtils.js
+++ src/utils/pathingUtils.js
@@ -1,5 +1,6 @@
 const { find_path } = require('dijkstrajs');
-const { singleQuery, endPool } = require('./databaseUtils');
+const { flatten } = require('lodash');
+const { singleQuery, endPool, fetchConnectionFromPool } = require('./databaseUtils');
 
 const pathingTest = () => {
   // A B C
@@ -94,22 +95,110 @@ const pathingTest = () => {
   console.log(find_path(myPathGraph, "A", "I"));
 }
 
-const getPathToSystem = async (sourceSystem, destinationSystem) => {
+// Returns ['a', 'b', 'c', 'd']
+const getPathToSystem = async (sourceSystem, destinationSystem, tableName = 'jumpPaths') => {
   // [{ systemA, systemB }]
-  const jumpPathsData = await singleQuery(`select systemA, systemB from jumpPaths`);
-  const graph = jumpPathsData.reduce((graphInProgress, { a, b }) => {
-    if (!graphInProgress[a]) {
-      graphInProgress[a] = {};
+  const jumpPathsData = await singleQuery(`select systemA, systemB from ${tableName}`);
+  // Get every endpoint
+  const allEndpoints = jumpPathsData.reduce((endpoints, { systemA, systemB }) => {
+    if (!endpoints.includes(systemA)) {
+      endpoints.push(systemA);
     }
-    if (!graphInProgress[b]) {
-      graphInProgress[b] = {};
+    if (!endpoints.includes(systemB)) {
+      endpoints.push(systemB);
     }
-    graphInProgress[a][b] = 1;
-    graphInProgress[b][a] = 1;
+    return endpoints;
+  }, []);
+
+  // Get where we can go from each endpoint
+  // dijkstrajs takes this format:
+  // {
+  //   a: {b: 10, d: 1},
+  //   b: {a: 1, c: 1, e: 1},
+  //   ...
+  // }
+  // Could probably add distance here but for now just make all jumps the same
+  const graph = allEndpoints.reduce((graphInProgress, endpoint) => {
+    const thisEndpointIsSystemA = jumpPathsData
+      .filter(({ systemA }) => systemA === endpoint)
+      .map(({ systemB }) => systemB);
+
+    const thisEndpointIsSystemB = jumpPathsData
+      .filter(({ systemB }) => systemB === endpoint)
+      .map(({ systemA }) => systemA);
+
+    const allTargetEndpoints = thisEndpointIsSystemA.reduce((runningList, potentialEndpoint) => {
+      if (!runningList.includes(potentialEndpoint)) {
+        runningList.push(potentialEndpoint);
+      }
+      return runningList;
+    }, [...thisEndpointIsSystemB]);
+
+    graphInProgress[endpoint] = allTargetEndpoints.reduce((list, endpoint) => {
+      list[endpoint] = 1;
+      return list;
+    }, {});
+
     return graphInProgress;
+
   }, {});
+
   return find_path(graph, sourceSystem, destinationSystem);
 }
+
+const testPathingFromDatabase = async () => {
+  // Add test data to database
+  const testTableName = 'jumpPathsTest';
+  let db;
+  try {
+    db = await fetchConnectionFromPool();
+    const currentTables = flatten(await db.query('SHOW TABLES'))
+      .map(({Tables_in_spacetraders}) => Tables_in_spacetraders);
+
+    if (currentTables.includes(testTableName)) {
+      await db.query('DROP TABLE ' + testTableName);
+    }
+    // Where you can go from each jump gate
+    await db.query(`CREATE TABLE ${testTableName} (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      systemA varchar(255),
+      systemB varchar(255),
+      PRIMARY KEY (id))`);
+
+    // Just a straight path for now
+    const testData = [
+      ['A', 'B'],
+      ['C', 'B'],
+      ['C', 'D'],
+      ['D', 'E'],
+      ['E', 'F'],
+      ['F', 'G'],
+    ];
+    await db.beginTransaction();
+    await testData.reduce(async (prevPromise, currentArray) => {
+      await prevPromise;
+      const queryString = `INSERT INTO ${testTableName} (systemA, systemB) VALUES ("${currentArray[0]}", "${currentArray[1]}")`
+      await db.query(queryString);
+    }, Promise.resolve());
+    await db.commit();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    db.end();
+  }
+
+  // Make sure that the pathing function returns the right path
+  const pathResult = await getPathToSystem('A', 'G', testTableName);
+  const expectedPathResult = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+  if (pathResult.length === expectedPathResult.length && pathResult.every(function(value, index) { return value === expectedPathResult[index]})) {
+    console.log('Test passed');
+  } else {
+    console.log('Test failed.');
+  }
+}
+// testPathingFromDatabase()
+//   .catch(console.error)
+//   .finally(endPool);
 
 module.exports = {
   getPathToSystem,

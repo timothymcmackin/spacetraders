3cd973d5d181e8ca5a46754cf67470a3f96a0933
Tim McMackin <timothymcmackin@yahoo.com>
2023-07-05 06:55:41 -0400

    mining and trading loop

--- src/mineSurveyLoop.js
+++ src/mineSurveyLoop.js
@@ -4,6 +4,8 @@ const {
   navigate,
   sellAll,
   travelToNearestMarketplace,
+  extractUntilFull,
+  survey,
 } = require('./utils/utils');
 const {
   getAvailableMiningShips,
@@ -37,7 +39,7 @@ const main = async () => {
     ));
 
     const allMinersPromise = getShipsByOrders('mine');
-    const allTradersPromise = getShipsByOrders('trade');
+    const allTradersPromise = getShipsByOrders('mineDelivery');
     const allScoutsPromise = getShipsByOrders('checkMarketplaces');
 
     // Mark scouts as active ships in the database
@@ -58,26 +60,13 @@ const main = async () => {
     }
     );
 
-    // const activeMinersPromise = allMinersPromise.then((minerSymbols) => {
-    //   if (minerSymbols && minerSymbols.length) {
-    //     return minerSymbols.reduce(async (currentListPromise, oneMinerSymbol) => {
-    //       const currentList = await currentListPromise;
-    //       const successfullyActivatedShip = await controlShip(oneMinerSymbol);
-    //       if (successfullyActivatedShip) {
-    //         // Successfully marked the ship as busy in the database
-    //         // So this ship is ready for use
-    //         currentList.push(oneMinerSymbol);
-    //       }
-    //       return currentList;
-    //     }, Promise.resolve([]));
-    //   }
-    // });
-
     // Send the scouts to marketplaces
-    const scoutLoopPromise = scoutLoop(activeScoutsPromise, systemSymbol);
+    scoutLoopPromise = scoutLoop(activeScoutsPromise, systemSymbol);
 
     // Send miners to mine
-    // const minerLoopPromise = minerLoop(activeMinersPromise, miningLocation);
+    mineLoopPromise = allMinersLoop();
+
+    // TODO send delivery ships to deliver
 
     await timer(loopWait);
     globalOrders = (await getGlobalOrders());
@@ -134,10 +123,135 @@ const sendScout = async (shipSymbol, marketplaceTarget) => {
 }
 
 // Send miners to mine until they are full and then transfer cargo to a waiting trader
-const minerLoop = async (activeMinersPromise, miningLocation) => {
-  // TODO
+const allMinersLoop = async () => {
+  // Get initial miners
+  const minerSymbols = await getShipsByOrders('mine');
+
+  // Take control by marking them as active in the database
+  var availableMiners = await minerSymbols.reduce(async (currentListPromise, oneMinerSymbol) => {
+    const currentList = await currentListPromise;
+    const successfullyActivatedShip = await controlShip(oneMinerSymbol);
+    if (successfullyActivatedShip) {
+      // Successfully marked the ship as busy in the database
+      // So this ship is ready for use
+      currentList.push(oneMinerSymbol);
+    }
+    return currentList;
+  }, Promise.resolve({}));
+
+  // Just to be sure
+  if (!availableMiners || availableMiners.length === 0) {
+    return [];
+  }
+
+  // Initiate a loop for each miner
+  return Promise.all(availableMiners.map((oneMinerSymbol) => minerLoop(oneMinerSymbol, miningLocation)));
 }
 
-main()
+const minerLoop = async (minerSymbol, miningLocation) => {
+  // Update active
+  await updateShipIsActive(minerSymbol);
+  // Go to miningLocation
+  await navigate({ symbol: minerSymbol }, miningLocation, 'mining');
+  // Mine until full
+  await extractUntilFull(minerSymbol);
+  await post(`/my/ships/${minerSymbol}/orbit`);
+  var { units } = await get(`/my/ships/${minerSymbol}/cargo`);
+  // Transfer cargo
+  while (units > 0) {
+
+    await transferLoop(minerSymbol, miningLocation);
+
+    // If we still have cargo, wait for a cargo ship
+    units = (await get(`/my/ships/${minerSymbol}/cargo`)).units;
+    if (units > 0) {
+      await timer(30);
+    }
+
+  }
+}
+
+const transferLoop = async (minerSymbol, miningLocation) => {
+
+  // Get symbols of cargo ships
+  const cargoShipSymbols = await getShipsByOrders('mineDelivery');
+  // Are there cargo ships with capacity in the same waypoint?
+  var cargoShipsAtWaypoint = await cargoShipSymbols.reduce(async (prevListPromise, oneCargoShipSymbol) => {
+    const prevList = await prevListPromise;
+    const { nav: { waypointSymbol }, cargo: cargoShipCargo } = await get(`/my/ships/${oneCargoShipSymbol}`);
+    if (waypointSymbol === miningLocation && cargoShipCargo.capacity > cargoShipCargo.units) {
+      prevList.push(oneCargoShipSymbol);
+    }
+    return prevList;
+  }, Promise.resolve([]));
+
+  // Find what to transfer to those cargo ships
+  while (cargoShipsAtWaypoint.length > 0) {
+    const currentCargoShipSymbol = cargoShipsAtWaypoint.pop();
+    // Get the capacity again in case someone else is transferring cargo to it
+    // But with so many awaits, we may have a race condition with another miner
+    const { capacity: oneCargoShipCapacity, units: oneCargoShipUnits } = await get(`/my/ships/${currentCargoShipSymbol}/cargo`);
+    var remainingCapacity = oneCargoShipCapacity - oneCargoShipUnits;
+    var { inventory } = await get(`/my/ships/${minerSymbol}/cargo`);
+
+    while (remainingCapacity > 0 && inventory.length > 0) {
+      const { symbol: symbolToTransfer, units: unitsToTransfer } = inventory.shift();
+      if (unitsToTransfer < remainingCapacity) {
+        // We can transfer all of the item
+        await post(`/my/ships/${minerSymbol}/transfer`, {
+          tradeSymbol: symbolToTransfer,
+          units: unitsToTransfer,
+          shipSymbol: currentCargoShipSymbol,
+        });
+        // Deduct from the cargo ship capacity
+        // TODO: may be a problem here; follow remainingCapacity to see if it's accurate
+        remainingCapacity -= unitsToTransfer;
+      } else {
+        // We can transfer only some of the item
+        await post(`/my/ships/${minerSymbol}/transfer`, {
+          tradeSymbol: symbolToTransfer,
+          units: remainingCapacity,
+          shipSymbol: currentCargoShipSymbol,
+        });
+        // Update inventory instead of trying to do the math here
+        inventory = (await get(`/my/ships/${minerSymbol}/cargo`)).inventory;
+      }
+    }
+
+  }
+}
+
+const deliveryLoop = async (traderSymbol, miningLocation) => {
+  // Go to mining location and await cargo
+  // Be sure you're in orbit
+  await navigate({ symbol: traderSymbol }, miningLocation);
+  var { units, capacity } = await get(`/my/ships/${traderSymbol}/cargo`);
+  survey(traderSymbol);
+  var maxWaitCycles = 20; // Wait 20 minutes for cargo
+  while (units < capacity && maxWaitCycles > 0) {
+    // Wait for cargo
+    updateShipIsActive(traderSymbol);
+    maxWaitCycles--;
+    await timer(60);
+    // Update cargo
+    const cargo = await get(`/my/ships/${traderSymbol}/cargo`);
+    units = cargo.units;
+    capacity = cargo.capacity;
+  }
+  var { inventory } = await get(`/my/ships/${traderSymbol}/cargo`);
+  if (inventory.length === 0) {
+    // Couldn't get any cargo
+    return;
+  }
+
+  // TODO Calculate single trip with the best price
+  // For now, just hard-code the market in this system
+  // b/c the starting system has an asteroid field and a market at the same waypoint
+  await post(`/my/ships/${traderSymbol}/dock`);
+  await sellAll(traderSymbol, true);
+  await post(`/my/ships/${traderSymbol}/orbit`);
+}
+
+deliveryLoop('KITE-1', miningLocation)
   .catch(console.error)
   .finally(endPool);

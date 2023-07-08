d057aad23b3b33bf67f2397dd1712224e24d8c15
Tim McMackin <timothymcmackin@yahoo.com>
2023-07-08 12:22:18 -0400

    oops

--- src/newAgent.js
+++ src/newAgent.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 const api = require('./utils/api');
 const { parseFile } = require('key-value-file');
+const axios = require('axios');
+const path = require('path');
+
+const client = axios.create({
+  baseURL: 'https://api.spacetraders.io/v2/',
+  timeout: 50000,
+});
 
 // Initialize new account after server reset
 const getNewAccount = async () => {
-  const { agent, token } = await api.post('/register', {
+  const response = await client.post('/register', {
     faction: process.env.SPACETRADERS_FACTION,
     symbol: process.env.SPACETRADERS_PREFIX,
     email: process.env.SPACETRADERS_EMAIL,
   });
 
-  const envFile = await parseFile('../.env');
+  const { agent, token } = response.data.data;
+  console.log(agent);
+  console.log(token);
+
+  const envFile = await parseFile(path.resolve(__dirname,'../.env'));
   await envFile
     .set('SPACETRADERS_TOKEN', token)
     .set('ACTIVE_SHIP', process.env.SPACETRADERS_PREFIX + '-1')
